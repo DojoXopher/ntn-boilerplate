3a09d7515adf74ba5346fa6b06665de889eff194
Xopher <operations@unitycbq.com>
2021-06-04 14:55:13 -0700

    Updated About page

--- pages/about.vue
+++ pages/about.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>
-      <h1>About CTX</h1>
-        <p>This is for me.</p>
-  </div>
+  <main>
+    <section class="self-center flex flex-col flex-1 items-center justify-center">
+      <h1 class="title text-center">About CTX</h1>
+      <h2 class="subtitle text-center">This is for me!</h2>
+    </section>
+
+  </main>
 </template>
 
 <script>
